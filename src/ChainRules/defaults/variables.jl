2fc3af2193044afcd24c1030f0204d20f3262027
Jarrett Revels <jarrettrevels@gmail.com>
2018-11-07 12:04:53 -0500

    implement sig macro

--- src/ChainRules/defaults/variables.jl
+++ src/ChainRules/defaults/variables.jl
@@ -3,8 +3,9 @@
 #####
 
 description(args::Tuple) = map(description, args)
-description(x::Real) = Scalar(RealDomain())
-description(x::Complex) = Scalar(ComplexDomain())
+description(::Function) = Func()
+description(::Real) = Scalar(RealDomain())
+description(::Complex) = Scalar(ComplexDomain())
 description(x::AbstractArray{<:Real}) = Tensor(RealDomain(), layout(x))
 description(x::AbstractArray{<:Complex}) = Tensor(ComplexDomain(), layout(x))
 
--- src/ChainRules/markup.jl
+++ src/ChainRules/markup.jl
@@ -43,9 +43,21 @@ output_count(sig::Signature) = sum(element_count, sig.output)
 
 #== `Func` ==#
 
+#=
+Note that initially, this used to be:
+
+```
 struct Func{S <: Signature} <: AbstractArgument
     signature::S
 end
+```
+
+While aesthetically nice (since it enables richer markup descriptions via nested
+function signatures), this capability seemed to confer no actual advantage in
+practice. On the contrary, it somewhat encourages over-specification in rule
+definition.
+=#
+struct Func <: AbstractArgument end
 
 element_count(::Func) = 0
 
@@ -64,3 +76,50 @@ end
 
 element_count(::Scalar) = 1
 element_count(t::Tensor) = element_count(t.layout)
+
+#####
+##### `@sig`
+#####
+
+macro sig(expr)
+    signature_type_from_expr(expr)
+end
+
+const MALFORMED_SIG_ERROR_MESSAGE = "Malformed expression given to `@sig`; see `@sig` docstring for proper format."
+
+function signature_type_from_expr(expr)
+    @assert(expr.head === :call && expr.args[1] === :→ && length(expr.args) === 3, MALFORMED_SIG_ERROR_MESSAGE)
+    input_types = map(parse_into_markup_type, split_infix_args(expr.args[2], :⊗))
+    output_types = map(parse_into_markup_type, split_infix_args(expr.args[3], :⊗))
+    return :(Signature{<:Tuple{$(input_types...)}, <:Tuple{$(output_types...)}})
+end
+
+split_infix_args(invocation::Symbol, ::Symbol) = (invocation,)
+
+function split_infix_args(invocation::Expr, op::Symbol)
+    if invocation.head === :call && invocation.args[1] === op
+        return (split_infix_args(invocation.args[2], op)..., invocation.args[3])
+    end
+    return (invocation,)
+end
+
+function parse_into_markup_type(x)
+    if x === :R
+        return :(Scalar{RealDomain})
+    elseif x === :C
+        return :(Scalar{ComplexDomain})
+    elseif x === :F
+        return :(Func)
+    elseif isa(x, Expr) && x.head === :vect && length(x.args) === 1
+        domain = x.args[1]
+        if domain === :R
+            return :(Tensor{RealDomain})
+        elseif domain === :C
+            return :(Tensor{ComplexDomain})
+        elseif domain === :F
+            return :(Tensor{Func})
+        end
+    end
+    error(string("Encountered unparseable signature element `", x, "`;",
+                 MALFORMED_SIG_ERROR_MESSAGE))
+end
