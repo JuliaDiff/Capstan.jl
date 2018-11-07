af0593ad61648a5ffa2f0424e40fc1f4101b28ac
Jarrett Revels <jarrettrevels@gmail.com>
2018-11-07 13:28:40 -0500

    remove now complete TODOs

--- src/ChainRules/ChainRules.jl
+++ src/ChainRules/ChainRules.jl
@@ -2,13 +2,6 @@ module ChainRules
 
 using Base.Broadcast: materialize, broadcasted
 
-#=
-TODO:
-    - obviously more `rule`s, `layout`s, and `description`s
-    - macros implementations: `@sig`, `@sigtype`
-    - interface overloadability design pass
-=#
-
 include("markup.jl")
 include("rules.jl")
 include("defaults/variables.jl")
