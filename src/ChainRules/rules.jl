9b7954e61f8415574ea9e317965aa03c2a07fd41
Jarrett Revels <jarrettrevels@gmail.com>
2018-11-08 16:43:49 -0500

    add comment explaining context-based interface for downstream usage of ChainRules by other ADs

--- src/ChainRules/rules.jl
+++ src/ChainRules/rules.jl
@@ -1,6 +1,28 @@
 #####
 ##### rules
 #####
+#=
+There's an idea at play here that's not made explicit in these fallbacks. In
+some weird ideal sense, the fallback for e.g. `forward_rule` should actually be
+"get the derivative via forward-mode AD". This is necessary to enable mixed-mode
+rules, where e.g. `forward_rule` is used within a `reverse_rule` definition. For
+example, `map`ped/`broadcast`ed functions may not themselves be forward-mode
+*primitives*, but are forward-mode *differentiable*.
+
+The problem is that ChainRules is (and should be) decoupled from any specific
+AD implementation. How, then, do we know which AD to fall back to when there
+isn't a primitive defined?
+
+I guess we could pass around some callback or something through to every rule to
+tell the fallback rule which AD to use, but...wait! That's exactly what Cassette
+allows you to do: pass around a context! Better yet, we don't have to add any of
+that machinery to ChainRules itself - downstream AD tools can use Cassette to
+inject their contexts post-hoc.
+
+Thus, that's the interface for downstream AD tools to support mixed-mode chain
+rules; just overload these `forward_rule`/`reverse_rule` fallbacks w.r.t. their
+own contexts via Cassette.
+=#
 
 forward_rule(::Signature, ::Any, ::Vararg{Any}) = nothing
 
